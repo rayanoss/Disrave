402747c5f676db326c8216ffd1d956e337769e34
Rayane <74737446+rayanoss@users.noreply.github.com>
2022-06-01 14:25:57 +0200

    commit

--- src/components/List/ModalInfo.js
+++ src/components/List/ModalInfo.js
@@ -1,19 +1,30 @@
 import React, {useEffect, useState} from 'react';
 import axios from 'axios';
+import ReactPlayer from 'react-player';
 const ModalInfo = ({movie}) => {
     const [movieGenres, setMovieGenres] = useState([]);
-    console.log(movieGenres)
+    const [key, setKey] = useState(null)
+    const url = "https://www.youtube.com/watch?v=" + key;
+    
     useEffect(() => {
         const fetchDataMovie = async () => {
             const res = await axios.get(`https://api.themoviedb.org/3/movie/${movie.id}?api_key=9abf0a996efeefc2cf9e9ab4f02bead8&language=fr-FR&append_to_response=videos`);
             setMovieGenres(res.data.genres); 
         } 
+
+        const fetchTrailerMovie = async () => {
+            const res = await axios.get(`https://api.themoviedb.org/3/movie/${movie.id}/videos?api_key=9abf0a996efeefc2cf9e9ab4f02bead8&language=fr-FR`);
+            setKey(res.data.results[res.data.results.length - 1].key)
+           
+        } 
+
         fetchDataMovie();
+        fetchTrailerMovie();
     }, [])
     return (
         <div className='modal-container'>
             <div className="movie-info">
-                <img src={"https://image.tmdb.org/t/p/w600_and_h900_bestv2" + movie.poster_path} alt="" />
+            <ReactPlayer url={url} controls={true} width={'100%'} height={'500px'}/>
                 <div className="text">
                     <p className='movie-title'>{movie.title}</p>
                     <ul className="list-genres">
